6b9d51a937daa60a002b2828ac25f852059631a7
SebastianRestrepo22 <restreposebastian306@gmail.com>
2026-03-26 11:15:20 -0500

    Arreglar correos

--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 
-// CONFIGURACIÓN ÚNICA Y ESTABLE PARA GMAIL (PUERTO 465)
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,           
+  secure: false,       
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
